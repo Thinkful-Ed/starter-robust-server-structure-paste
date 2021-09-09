f7fa47deaf3b942a2a3aaacc393972eb352cfea5
gabrielsanchez <gabrielsz87@gmail.com>
2021-09-09 15:49:07 -0500

    add pastes-data.js and route handlers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
+const pastes = require("./data/pastes-data");
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next(`Paste id not found: ${PasteId}`);
+  }
+});
+
+app.use("/pastes", (req, res) => {
+  res.json({ data: pastes });
+});
 
 // Not found handler
 app.use((request, response, next) => {
---
+++ src/data/pastes-data.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Hello",
+    syntax: "None",
+    expiration: 10,
+    exposure: "private",
+    text: "Hello World!"
+  },
+  {
+    id: 2,
+    name: "Hello World in Python",
+    syntax: "Python",
+    expiration: 24,
+    exposure: "public",
+    text: "print(Hello World!)"
+  },
+  {
+    id: 3,
+    name: "String Reverse in JavaScript",
+    syntax: "Javascript",
+    expiration: 24,
+    exposure: "public",
+    text: "const stringReverse = str => str.split('').reverse().join('');"
+  },
+  {
+    id: 4,
+    name: "Print file sizes in Perl",
+    syntax: "Perl",
+    expiration: 24,
+    exposure: "public",
+    text: "ls -lAF | perl -e ’while (<>) { next if /^[dt]/; print +(split)[4], '\n' } ’"
+  }
+];
