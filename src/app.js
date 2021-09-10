f217b491e6e45f19689bb017cf1fc806bacb6c6c
gabrielsanchez <gabrielsz87@gmail.com>
2021-09-10 10:09:38 -0500

    add GET and POST

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const pastes = require("./data/pastes-data");
 
+app.use(express.json());
+
 app.use("/pastes/:pasteId", (req, res, next) => {
   const { pasteId } = req.params;
   const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
@@ -13,8 +15,30 @@ app.use("/pastes/:pasteId", (req, res, next) => {
   }
 });
 
-app.use("/pastes", (req, res) => {
+app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
+});
+
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+app.post("/pastes", (req, res, next) => {
+  const { data: { name, syntax, exposure, expiration, text } = {} } = req.body;
+  if(text){
+      const newPaste = {
+        id: ++lastPasteId, // Increment last ID, then assign as the current ID
+        name: name,
+        syntax: syntax,
+        exposure: exposure,
+        expiration: expiration,
+        text: text,
+      };
+      pastes.push(newPaste);
+      res.status(201).json({ data: newPaste });
+  } else {
+      res.sendStatus(400);
+  }
 });
 
 // Not found handler
