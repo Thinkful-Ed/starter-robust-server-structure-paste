8903d644faf5a8e543d7e476bed18e20b119fbf6
dylan-adams-webdev <dadams31@asu.edu>
2022-03-21 23:21:37 -0700

    partial completion

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const pastes = require("./data/pastes-data");
 
+app.use(express.json());
+
 app.use("/pastes/:pasteId", (req, res, next) => {
   const { pasteId } = req.params;
   const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
@@ -9,12 +11,38 @@ app.use("/pastes/:pasteId", (req, res, next) => {
   if (foundPaste) {
     res.json({ data: foundPaste });
   } else {
-    next(`Paste id not found: ${PasteId}`);
+    next(`Paste id not found: ${pasteId}`);
   }
 });
 
-app.use("/pastes", (req, res) => {
+app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
+});
+
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+app.post("/pastes", (req, res, next) => {
+	const {
+		data: {
+			name,
+			syntax,
+			exposure,
+			expiration,
+			text,
+			user_id
+		} = {}
+	} = req.body;
+	const newPaste = {
+		id: ++lastPasteId,
+		name,
+		syntax,
+		exposure,
+		expiration,
+		text,
+		user_id,
+	};
+	pastes.push(newPaste);
+	res.json({ data: newPaste });
 });
 
 // Not found handler
